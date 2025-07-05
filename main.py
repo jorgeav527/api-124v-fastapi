faa1e11eb21e5180e5e168c630d2f69c1a1d0697
jorgeav527 <jorgeav527@gmail.com>
2025-07-04 19:51:47 -0500

    get all posts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Form
 from pymongo import MongoClient
 from pydantic import BaseModel, Field
 from datetime import datetime
@@ -30,12 +30,47 @@ class PostCreate(BaseModel):
 def index():
     return {"message": "Welcome to the API, Upgrade"}
 
+@app.get("/post")
+def get_all_post(db=Depends(get_db)):
+    posts = []
+    docs = db["posts"].find()
+    print(docs)
+    for post in docs:
+        posts.append({
+            "id": str(post["_id"]),
+            "title": post["title"],
+            "content": post["content"],
+            "created": post.get("created", datetime.now()).isoformat()
+        })
+    return {"total": len(posts) ,"posts": posts}
+
 
 @app.post("/post/create-json-data")
 def create_one_post_json_data(post: PostCreate, db = Depends(get_db)):
     new_post = {
         "title": post.title,
         "content": post.content,
+        "created": datetime.now()
+    }
+    result = db["posts"].insert_one(new_post)
+    created_post = db["posts"].find_one({"_id": result.inserted_id})
+
+    return {
+        "id": str(created_post["_id"]),
+        "title": created_post["title"],
+        "content": created_post["content"],
+        "created": created_post["created"].isoformat()
+    }
+
+@app.post("/post/create-form-data")
+def create_one_post_form_data(
+    title: str = Form(..., min_length=1, max_length=20),
+    content: str = Form(..., min_length=1, max_length=255),
+    db=Depends(get_db)
+):
+    new_post = {
+        "title": title,
+        "content": content,
         "created": datetime.now()
     }
     result = db["posts"].insert_one(new_post)
